b59361c29f2d4a013e878c8e8e247ba2412f569c
youssefsakini <youssef0sakini@gmail.com>
2022-10-14 19:45:14 +0100

    Setup all pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { AboutMeComponent } from './pages/about-me/about-me.component';
+import { ContactMeComponent } from './pages/contact-me/contact-me.component';
+import { MyProjectComponent } from './pages/my-project/my-project.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AboutMeComponent,
+    ContactMeComponent,
+    MyProjectComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/pages/my-project/my-project.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-project',
+  templateUrl: './my-project.component.html',
+  styleUrls: ['./my-project.component.scss']
+})
+export class MyProjectComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
