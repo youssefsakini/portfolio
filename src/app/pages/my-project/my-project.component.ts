c2cffbe3f4a3be84d79546085bd5cbc6653663b6
youssefsakini <youssef0sakini@gmail.com>
2022-11-18 16:12:32 +0100

    Add footer

--- src/app/pages/my-project/my-project.component.ts
+++ src/app/pages/my-project/my-project.component.ts
@@ -7,6 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyProjectComponent implements OnInit {
 
+  projects:any = [
+    {
+      id:1,
+      name:'MEDIEXPERTS APPLICATION',
+      technologie:'Laravel/VueJs'
+    },
+    {
+      id:2,
+      name:'ATTAWFIQ MICRO-FINANCE APP',
+      technologie:'Angular/NodeJs : ExpressJs'
+    },
+    {
+      id:3,
+      name:'ANGRY-SKELETON WEB SITE',
+      technologie:'Laravel/VueJs'
+    },
+    {
+      id:4,
+      name:'ROSACOLOR WEB SITE',
+      technologie:'NextJs'
+    },
+  ]
   constructor() { }
 
   ngOnInit(): void {
