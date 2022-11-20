1b5e653cf46fa485657567d630f0dec7eb1ec69c
youssefsakini <youssef0sakini@gmail.com>
2022-11-20 14:24:26 +0100

    Add contact me form and style

--- src/app/pages/my-project/my-project.component.ts
+++ src/app/pages/my-project/my-project.component.ts
@@ -12,30 +12,33 @@ export class MyProjectComponent implements OnInit {
       id:1,
       name:'MEDIEXPERTS APPLICATION',
       technologie:'Laravel/VueJs',
-      image: './../../../assets/images/mxperts.png'
+      image: './../../../assets/images/mxperts.png',
+      link: 'https://mediexperts.ma/'
     },
     {
       id:2,
       name:'ATTAWFIQ MICRO-FINANCE APP',
       technologie:'Angular/NodeJs : ExpressJs',
-      image: './../../../assets/images/TAWFIQ.jpg'
+      image: './../../../assets/images/TAWFIQ.jpg',
+      link: 'https://loyer.netlify.app/'
     },
     {
       id:3,
       name:'ANGRY-SKELETON WEB SITE',
       technologie:'Angular',
-      image: './../../../assets/images/skeleton1.jpeg'
+      image: './../../../assets/images/skeleton1.jpeg',
+      link: 'https://angryskeleton.netlify.app/'
     },
     {
       id:4,
       name:'ROSACOLOR WEB SITE',
       technologie:'NextJs',
-      image: './../../../assets/images/rosacolor.png'
+      image: './../../../assets/images/rosacolor.png',
+      link: 'https://rosacolor-front.vercel.app/'
     },
   ]
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
